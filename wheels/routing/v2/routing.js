a16c265aaef060b3e82b5b61f068e31144c79776
timothy-tianmu <tianmu_19970924@163.com>
2021-05-25 00:23:33 +0800

    routing v2 match

--- wheels/routing/v2/routing.js
+++ wheels/routing/v2/routing.js
@@ -45,51 +45,91 @@ module.exports = class Router {
         );
     }
 
-    buildPathRegx(ruleRegx, pathRule) {
+    /**
+    * ruleRegx: 上文中的匹配路径规则的正则表达式
+    * pathRule: 例 /user/{toInt(radix=10):id}/{toFloat:num}
+    */
+    buildPathRuleRegx(ruleRegx, pathRule) {
         let pos = 0
         let end = pathRule.length
-        const data = []
+        const matches = []
         while (pos < end) {
             const m = ruleRegx.exec(pathRule)
             if (m) {
-                data.push(m.groups)
+                matches.push(m.groups)
                 pos = m.index + m[0].length
             }
         }
-        return data
-    }
-
-    addRoute(pathRule, handler) {
-        const data = this.buildPathRegx(this.ruleRegx, pathRule)
-        const routeRule = {
+        const pathContext = {
             converters: {},
             regx: "",
-            handler,
         }
-        data.forEach(o => {
+        matches.forEach(o => {
             if (o.static) {
+                // 匹配的路径规则中的静态部分，直接拼接到正则中
                 pathContext.regx += o.static
             }
             if (o.converter) {
-                pathContext.converters['variable'] = this.converters[o.converter]
+                // 保存变量名与转换函数的映射
+                // 之后匹配到变量对应路径中的部分时，直接使用变量名索引转换函数进行转换
+                pathContext.converters[
+                    o.variable
+                ] = this.converters[o.converter]
             }
-            pathContext.regx += "{(?<variable>[a-zA-Z_][a-zA-Z0-9_]*)}"
+            // 变量捕获
+            pathContext.regx += `(?<${o.variable}>[a-zA-Z_][a-zA-Z0-9_]*)`
         })
-        this.routes.push(routeRule);
+        return pathContext
+    }
+
+    addRoute(pathRule, handler) {
+        const context = this.buildPathRuleRegx(this.ruleRegx, pathRule)
+        this.routes.push({
+            path: pathRule,
+            context,
+            handler,
+        });
     }
 
     match(path) {
         this.routes.forEach(r => {
-            const contexts = this.buildPathRegx(new RegExp(r.regx, "g"), path)
-            if (contexts) {
-                contexts.map(c => {
-                    const conv = r.converters[c.variable]
-                    let v;
+            const c = r.context;
+            const h = r.handler;
+            const regx = new RegExp(c.regx, "g")
+
+            let pos = 0
+            let end = path.length
+            const matches = []
+            while (pos < end) {
+                const m = regx.exec(path)
+                if (m) {
+                    matches.push(m.groups)
+                    pos = m.index + m[0].length
+                }
+            }
+
+            let vars = null
+            matches.forEach(m => {
+                if (!vars) vars = {}
+                vars = {
+                    ...vars,
+                    ...m,
+                }
+            })
+
+            if (vars) {
+                Object.keys(vars).forEach(name => {
+                    const conv = c.converters[name]
                     if (conv) {
-                        v = conv()
+                        vars[name] = conv(vars[name])
                     }
                 })
+                return {
+                    handler,
+                    vars,
+                }
             }
+            return null;
         })
     }
 };
